19e9c1b04a34607ecbfa191ba0cf7da579897adb
WingerZeng <WingerZeng@zju.edu.cn>
2021-11-16 16:34:29 +0800

    Bug Fix

--- Source/FPSHero/FPSHero.Build.cs
+++ Source/FPSHero/FPSHero.Build.cs
@@ -8,6 +8,6 @@ public class FPSHero : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" ,"PhysicsCore"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysicsCore"});
     }
 }
